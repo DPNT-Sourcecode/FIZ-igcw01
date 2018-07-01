414bc680e8929de15b3e1849e381bf02a10131b1
sbx_user1061 <sbx_user1061@ip-10-11-151-198.eu-west-2.compute.internal>
2018-07-01 09:47:22 +0000

    Sun Jul 01 09:47:22 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -94,17 +94,6 @@ public class FizzBuzzSolutionTest {
         Assert.assertTrue(output.equals("fizz fake deluxe"));
     }
 
-
-    @Test
-    public void deluxeOnly() {
-
-        FizzBuzzSolution fbs = new FizzBuzzSolution();
-        String output = fbs.fizzBuzz(22);
-
-        Assert.assertTrue(output.equals("deluxe"));
-
-    }
-
     //  - If a "deluxe" number is odd, we should call him "fake deluxe" A number cannot be both "deluxe" and "fake deluxe" at the same time
     @Test
     public void fakeDeluxeOnly() {
@@ -114,16 +103,6 @@ public class FizzBuzzSolutionTest {
 
         Assert.assertTrue(output.equals("fake deluxe"));
     }
-
-
-
-
-    //   - The old rules for "deluxe" do not apply.
-    //  - A number is "fizz deluxe" if it is divisible by 3 AND it has a 3 in it
-    //  - A number is "buzz deluxe" if it is divisible by 5 AND it has a 5 in it
-    //  - We should still say "fake deluxe" if the "deluxe" number is odd
-    //  - A number can be "fizz", "buzz" and "deluxe" at the same time. If this happens then write "fizz buzz deluxe"
-    //  - All the previous rules for "fizz" and "buzz" are still valid
 
 
 }

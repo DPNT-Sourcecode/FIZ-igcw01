0c40cdab1621495113e43a2c74c4cf826a249a8c
sbx_user1061 <sbx_user1061@ip-10-11-151-198.eu-west-2.compute.internal>
2018-07-01 10:02:22 +0000

    Sun Jul 01 10:02:22 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,44 +1,47 @@
 package befaster.solutions.FIZ;
 
-import java.util.HashSet;
-import java.util.Set;
 
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
 
-        // fizz deluxe divisible by 3 AND it has a 3 in it
-        // buzz deluxe  divisible by 5 AND it has a 5 in it
-
+        boolean isDeluxe = false;
         boolean isFakeDeluxe = number % 2 == 0 ? false : true; // fake deluxes are odd numbers
-
-        String deluxeVal = "deluxe";
-        if(isFakeDeluxe){
-            deluxeVal = "fake deluxe";
-        }
-
 
         boolean divisibleBy3 = number % 3 ==0 ? true : false;
         boolean containsChar3 = String.valueOf(number).contains("3");
-
         boolean divisibleBy5 = number % 5 ==0 ? true : false;
         boolean containsChar5 = String.valueOf(number).contains("5");
 
+        if(!isFakeDeluxe && (divisibleBy3 && containsChar3)){
+            isDeluxe = true;
+        }
+        if(!isFakeDeluxe && (divisibleBy5 && containsChar5)){
+            isDeluxe = true;
+        }
+
+        String deluxeVal = "";
+        if(isFakeDeluxe){
+            deluxeVal = " fake deluxe";
+        }else if(isDeluxe){
+            deluxeVal = " deluxe";
+        }
 
         if((divisibleBy3 || containsChar3) && (divisibleBy5 || containsChar5)){
-            return String.format("fizz buzz %s", deluxeVal);
+
+            return String.format("fizz buzz%s", deluxeVal);
         }
 
         if(divisibleBy3 || containsChar3){
-            return  String.format("fizz %s", deluxeVal);
+            return  String.format("fizz%s", deluxeVal);
         }
 
         if(divisibleBy5 || containsChar5){
-            return  String.format("buzz %s", deluxeVal);
+            return  String.format("buzz%s", deluxeVal);
         }
 
-        if(isFakeDeluxe){
-            return  deluxeVal;
+        if(isFakeDeluxe || isDeluxe){
+            return  deluxeVal.trim();
         }
 
         return String.valueOf(number);
